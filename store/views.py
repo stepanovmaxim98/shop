d4ac186b995004050deb23cb0722b597a1b2e58b
hexamind <stepanovmaxim98@yandex.ru>
2026-05-06 15:30:03 +0300

    Добавили все пресдтавления (views)

--- store/views.py
+++ store/views.py
@@ -14,3 +14,24 @@ def product_detail(request, pk):
     return render(request, "store/product_detail.html", {"product": product})
 
 
+# Добавить в корзину
+def add_to_cart(request, pk):
+    product = get_object_or_404(Product, pk)
+    cart, created = Cart.objects.get_or_creat(user=request.user)
+    item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+    item.quantity += 1
+    item.save()
+
+    return redirect("cart")
+
+
+# Просмотр корзирны
+def cart_view(request):
+    cart, created = Cart.objects.get_or_creat(user=request.user)
+    items = cart.items.all()
+    total_items = sum(item.quantity for item in items)
+    return render(
+        request,
+        "store/cart.html",
+        {"cart": cart, "items": items, "total_items": total_items},
+    )
