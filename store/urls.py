fac64ec5a1ccf306b1c532f3efa8cced3793f71a
hexamind <stepanovmaxim98@yandex.ru>
2026-05-06 15:35:45 +0300

    добавили пути к стрнаицам

---
+++ store/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.product_list, name="product_list"),
+    path("product/<int:pk>", views.product_detail, name="product_detail"),
+    path("add-to-cart/<int:pk>", views.add_to_cart, name="add_to_cart"),
+    path("cart/", views.cart_view, name="cart"),
+]
