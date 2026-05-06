0d1f98ed6b81b3ce8df48bd52098fad1f5480f00
hexamind <stepanovmaxim98@yandex.ru>
2026-05-06 16:05:58 +0300

    добавили шаблоны и зарегистрировали модели

--- shop/settings.py
+++ shop/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'store'
+    "store",
 ]
 
 MIDDLEWARE = [
@@ -116,3 +116,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
